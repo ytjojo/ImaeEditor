c81b1c6554e5fdb3ef0fc9980e7e8174aca28bf4
杨腾蛟 <yangtengjiao@wogoo.com>
2023-11-02 11:28:20 +0800

    [feature]保存成功后刷新相册

---
+++ imageEditor/src/main/java/me/minetsh/imaging/core/util/ImageSaveNotifyAblumUtil.java
@@ -0,0 +1,130 @@
+package me.minetsh.imaging.core.util;
+
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.text.TextUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class ImageSaveNotifyAblumUtil {
+
+
+    public static void onSaveTaskDone(Context context, String saveFilePath) {
+
+        ablumUpdate(context, saveFilePath);
+        File file = new File(saveFilePath);
+        ContentResolver localContentResolver = context.getContentResolver();
+        ContentValues localContentValues = getImageContentValues(file, System.currentTimeMillis());
+        localContentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, localContentValues);
+
+        Intent localIntent = new Intent("android.intent.action.MEDIA_SCANNER_SCAN_FILE");
+        final Uri localUri = Uri.fromFile(file);
+        localIntent.setData(localUri);
+        context.sendBroadcast(localIntent);
+
+
+    }
+
+
+    public static ContentValues getImageContentValues(File paramFile, long paramLong) {
+        ContentValues localContentValues = new ContentValues();
+        localContentValues.put("title", paramFile.getName());
+        localContentValues.put("_display_name", paramFile.getName());
+        localContentValues.put("mime_type", "image/jpeg");
+        localContentValues.put("datetaken", Long.valueOf(paramLong));
+        localContentValues.put("date_modified", Long.valueOf(paramLong));
+        localContentValues.put("date_added", Long.valueOf(paramLong));
+        localContentValues.put("orientation", Integer.valueOf(0));
+        localContentValues.put("_data", paramFile.getAbsolutePath());
+        localContentValues.put("_size", Long.valueOf(paramFile.length()));
+        return localContentValues;
+    }
+
+
+    /**
+     * 将图片文件加入到相册
+     *
+     * @param context
+     * @param dstPath
+     */
+    public static void ablumUpdate(final Context context, final String dstPath) {
+        if (TextUtils.isEmpty(dstPath) || context == null)
+            return;
+
+        ContentValues values = new ContentValues(2);
+        String extensionName = getExtensionName(dstPath);
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/" + (TextUtils.isEmpty(extensionName) ? "jpeg" : extensionName));
+        values.put(MediaStore.Images.Media.DATA, dstPath);
+        context.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+    }
+
+    // 获取文件扩展名
+    public static String getExtensionName(String filename) {
+        if ((filename != null) && (filename.length() > 0)) {
+            int dot = filename.lastIndexOf('.');
+            if ((dot > -1) && (dot < (filename.length() - 1))) {
+                return filename.substring(dot + 1);
+            }
+        }
+        return "";
+    }
+
+
+    public static Dialog getLoadingDialog(Context context, int titleId,
+                                          boolean canCancel) {
+        return getLoadingDialog(context, context.getString(titleId), canCancel);
+    }
+
+    public static Dialog getLoadingDialog(Context context, String title,
+                                          boolean canCancel) {
+        ProgressDialog dialog = new ProgressDialog(context);
+        dialog.setCancelable(canCancel);
+        dialog.setMessage(title);
+        return dialog;
+    }
+
+    public static boolean saveBitmap(Bitmap bitmap, String path) {
+
+        if (!TextUtils.isEmpty(path)) {
+            File parentFile = new File(path).getParentFile();
+            if (!parentFile.exists()) {
+                parentFile.mkdirs();
+            }
+            if (bitmap != null) {
+                FileOutputStream fout = null;
+                try {
+                    fout = new FileOutputStream(path);
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 90, fout);
+                    return true;
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (fout != null) {
+                        try {
+                            fout.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+
+            }
+        }
+        return false;
+    }
+}
+
+
+
